0b1ef0a46920d164d5e47281c43999b8362e5324
Sergio Padron <srpadron@gmail.com>
2022-08-22 10:58:14 +0100

    Add dependencies

--- Modules/ModuleA/ModuleA/Package.swift
+++ Modules/ModuleA/ModuleA/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ModuleA",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +13,15 @@ let package = Package(
             targets: ["ModuleA"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ModuleA",
-            dependencies: []),
+            dependencies: ["Alamofire", .product(name: "RxCocoa", package: "RxSwift"), "RxSwift"]),
         .testTarget(
             name: "ModuleATests",
             dependencies: ["ModuleA"]),
--- Modules/ModuleA/ModuleA/Sources/ModuleA/ModuleA.swift
+++ Modules/ModuleA/ModuleA/Sources/ModuleA/ModuleA.swift
@@ -1,3 +1,7 @@
+import Alamofire
+import RxCocoa
+import RxSwift
+
 public struct ModuleA {
     public private(set) var text = "Hello, World!"
 
