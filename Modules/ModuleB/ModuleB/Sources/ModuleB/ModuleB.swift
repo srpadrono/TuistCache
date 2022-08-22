b5a4f36f42ab239b669d9c188cb7f621f94b2044
Sergio Padron <srpadron@gmail.com>
2022-08-22 15:53:05 +0100

    test

--- Modules/ModuleB/ModuleB/Sources/ModuleB/ModuleB.swift
+++ Modules/ModuleB/ModuleB/Sources/ModuleB/ModuleB.swift
@@ -1,6 +1,6 @@
 public struct ModuleB {
     public private(set) var text = "Module B"
-    public private(set) var version = "V2"
+    public private(set) var version = "V3"
 
     public init() { }
 }
